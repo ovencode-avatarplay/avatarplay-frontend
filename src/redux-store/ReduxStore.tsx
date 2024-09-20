db8c2eb220a18e2e1ca369b267ceeca31a88794e
tr8425 <tr8425@naver.com>
2024-09-20 10:44:43 +0900

    Redux 베이스

--- src/redux-store/ReduxStore.tsx
+++ src/redux-store/ReduxStore.tsx
@@ -1,65 +1,34 @@
-import { configureStore, createSlice, PayloadAction } from '@reduxjs/toolkit';
+// Third-party Imports
+import {combineReducers, configureStore } from '@reduxjs/toolkit'
+
+// Slice Imports
+import storage from 'redux-persist/lib/storage' // localStorage를 사용
+
+// Sliced Imports
+import sampleReducer from '@/redux-store/slices/ReduxSample'
 
 
-//#region  Sample Redux 
-// Redux 사용 GuideLine 입니다. 실제 사용 X
-export interface ReduxSampleData {
-    key : number;
-    data : string;
+// redux-persist (간단 요약 : 새로고침 해도 데이터 유지)   https://www.codenary.co.kr/discoveries/9170
+import { persistStore, persistReducer } from 'redux-persist'
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['sampleReducer']
 }
 
-// ReduxState 인터페이스
-interface ReduxSampleState {
-    redux: Record<number, ReduxSampleData>; // C#의 Dictionary처럼 key-value 형태
-}
+const reducers = combineReducers({
+    sampleReducer
+})
 
-const initialState: ReduxSampleState = {
-    redux: {}, // 빈 객체로 초기화
-};
+const persistedReducer = persistReducer(persistConfig, reducers)
 
-// reduxSlice 생성
-const reduxSlice = createSlice({
-    name: 'reduxSample',
-    initialState,
-    reducers: {
-        // Data 추가
-        AddSampleData: (state, action: PayloadAction<ReduxSampleData>) => {
-            const sampleData = action.payload;
-            state.redux[sampleData.key] = sampleData; // key를 기준으로 데이터 추가
-        },
-
-        // Data 수정
-        SetSampleData: (state, action: PayloadAction<ReduxSampleData>) => {
-            const sampleData = action.payload;
-            if (state.redux[sampleData.key]) {
-                state.redux[sampleData.key] = sampleData; // 해당 key에 있는 데이터 수정
-            }
-        },
-
-        // Data 삭제 (key 타입은 number)
-        RemoveSampleData: (state, action: PayloadAction<number>) => {
-            const key = action.payload;
-            if (state.redux[key]) {
-                delete state.redux[key]; // 해당 key의 데이터 삭제
-            }
-        },
-    },
-});
-
-// 액션 및 리듀서 내보내기
-export const { AddSampleData, SetSampleData, RemoveSampleData } = reduxSlice.actions;
-export const reduxSampleReducer = reduxSlice.reducer;
-
-//#endregion
-
-// Redux 스토어 구성
 export const store = configureStore({
-    reducer: {
-        // reduxSample : reduxSampleReducer,
+    reducer: persistedReducer, // Persisted reducer를 사용
+    middleware: getDefaultMiddleware => getDefaultMiddleware({ serializableCheck: false }),
+})
 
-    },
-});
-
+export const persistor = persistStore(store);
 
 // RootState와 AppDispatch 타입 정의
 export type RootState = ReturnType<typeof store.getState>;
---
+++ src/redux-store/slices/ReduxSample.ts
@@ -0,0 +1,59 @@
+// Third-party Imports
+import { createSlice } from '@reduxjs/toolkit'
+import type { PayloadAction } from '@reduxjs/toolkit'
+
+// Type Imports 
+// import type {Type} from '@/loc/loc/Type'
+
+// 다른 컴포넌트에서도 사용 가능하게 외부에서 선언한 Type을 받아서 쓰는것을 권장하지만, 일단은 인터페이스를 여기서 선언
+export interface ReduxSampleData {
+    key : number;
+    data : string;
+}
+// ReduxState 인터페이스
+interface ReduxSampleState {
+    redux: Record<number, ReduxSampleData>; // C#의 Dictionary처럼 key-value 형태
+}
+
+// Data Imports
+// import {db} from '@/loc/loc/db'
+
+// 받아온 데이터를 넣어주거나 초기값을 지정해주기.
+const initialState: ReduxSampleState = {
+    redux: {}, // 빈 객체로 초기화
+};
+
+export const reduxSampleSlice = createSlice({
+    name : 'sample',
+    initialState : /*db*/ initialState,    // 받아온 데이터를 넣어주거나 아예 초기값을 넣어주기.
+    reducers :{
+         // Data 추가
+         AddSampleData: (state, action: PayloadAction<ReduxSampleData>) => {
+            const sampleData = action.payload;
+            state.redux[sampleData.key] = sampleData; // key를 기준으로 데이터 추가
+        },
+
+        // Data 수정
+        SetSampleData: (state, action: PayloadAction<ReduxSampleData>) => {
+            const sampleData = action.payload;
+            if (state.redux[sampleData.key]) {
+                state.redux[sampleData.key] = sampleData; // 해당 key에 있는 데이터 수정
+            }
+        },
+
+        // Data 삭제 (key 타입은 number)
+        RemoveSampleData: (state, action: PayloadAction<number>) => {
+            const key = action.payload;
+            if (state.redux[key]) {
+                delete state.redux[key]; // 해당 key의 데이터 삭제
+            }
+        },
+
+        // reducer는 필요에 따라 구현
+    }
+})
+
+
+export const { AddSampleData, SetSampleData, RemoveSampleData } = reduxSampleSlice.actions;
+export const reduxSampleReducer = reduxSampleSlice.reducer;
+export default reduxSampleSlice.reducer;
