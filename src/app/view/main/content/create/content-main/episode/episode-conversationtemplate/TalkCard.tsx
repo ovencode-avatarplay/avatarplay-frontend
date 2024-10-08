2ac1a874d066690a04f22d5c9f784179a97bce93
BaeHyeonSeong <gustjd8121@gmail.com>
2024-10-08 09:46:24 +0900

    episode-conversationtemplate 개발중

--- src/app/view/main/content/create/content-main/episode/episode-conversationtemplate/TalkCard.tsx
+++ src/app/view/main/content/create/content-main/episode/episode-conversationtemplate/TalkCard.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Card, CardContent, FormControl, InputLabel, Select, MenuItem, SelectChangeEvent, Button, CardHeader, Avatar, IconButton, Typography, TextField } from '@mui/material';
 import styles from './TalkCard.module.css';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import PhoneInTalkIcon from '@mui/icons-material/PhoneInTalk';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 interface TalkCardProps {
     card: {
         id: number;
@@ -37,25 +39,41 @@ const TalkCard: React.FC<TalkCardProps> = ({ card, selectedPriority, priorities,
                 </FormControl>
                 <Button className={styles.button1}> delete </Button>
             </div>
-            <Card className={styles.cardRect}>
-                <Card className={styles.cardTop}>
-                    <Avatar src="/path/to/image.jpg" alt="Avatar" className={styles.cardTopImage} />
-                    <Typography variant="subtitle2" className={styles.cardTopText}>
-                        User's Talk
-                    </Typography>
-                    <AddCircleOutlineIcon></AddCircleOutlineIcon>
-                </Card>
-                <Card>
-                    <TextField
-                        hiddenLabel
-                        id="filled-hidden-label-small"
-                        defaultValue="Small"
-                        variant="filled"
-                        size="small"
-                    />
-                </Card>
-            </Card>
 
+            <div className={styles.divBox}>
+                <div className={styles.innerBox}>
+                    <Card className={styles.cardTop}>
+                        <Avatar src="/path/to/image.jpg" alt="Avatar" className={styles.cardTopImage} />
+                        <Typography variant="subtitle2" className={styles.cardTopText}>
+                            User's Talk
+                        </Typography>
+                        <IconButton>
+                            <AddCircleOutlineIcon></AddCircleOutlineIcon>
+                        </IconButton>
+
+                    </Card>
+                    <Card className={styles.cardRect}>
+
+                        <Card className={styles.rowContent}>
+                            <IconButton>
+                                <PhoneInTalkIcon />
+                            </IconButton>
+                            <TextField
+                                hiddenLabel
+                                id="filled-hidden-label-small"
+                                defaultValue="Small"
+                                variant="filled"
+                                size="small"
+                            />
+                            <IconButton>
+                                <DeleteForeverIcon></DeleteForeverIcon>
+                            </IconButton>
+                        </Card>
+                    </Card>
+                </div>
+
+
+            </div>
         </Card >
     );
 };
