242955b9ce94b5868b1eafbd15a5bb461be5ae8e
XeDnA01 <ojb1112@gmail.com>
2024-10-10 16:51:16 +0900

    애피소드아이디 추가

--- src/redux-store/slices/chatting.ts
+++ src/redux-store/slices/chatting.ts
@@ -4,21 +4,25 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 const initialState = {
   contentName: '',
   episodeName: '',
+  episodeId: 0,
 };
 
 export const chattingSlice = createSlice({
   name: 'chatting',
   initialState,
   reducers: {
-    setContentName: (state, action: PayloadAction<string>) => {
-      state.contentName = action.payload;  // action.payload에서 contentName을 업데이트
-    },
-    setEpisodeName: (state, action: PayloadAction<string>) => {
-      state.episodeName = action.payload;  // episodeName을 업데이트하는 reducer
-    },
-  },
+        setContentName: (state, action: PayloadAction<string>) => {
+        state.contentName = action.payload;  // action.payload에서 contentName을 업데이트
+        },
+        setEpisodeName: (state, action: PayloadAction<string>) => {
+        state.episodeName = action.payload;  // episodeName을 업데이트하는 reducer
+        },
+        setEpisodeId: (state, action: PayloadAction<number>) => {
+            state.episodeId = action.payload;  // episodeName을 업데이트하는 reducer
+        }
+    }
 });
 
 // 액션과 리듀서 내보내기
-export const { setContentName, setEpisodeName } = chattingSlice.actions;
+export const { setContentName, setEpisodeName, setEpisodeId } = chattingSlice.actions;
 export default chattingSlice.reducer;
