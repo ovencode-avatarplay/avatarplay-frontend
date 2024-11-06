556a701d3461e9797c9d7c32e56c805f7021c7a7
tr8425 <tr8425@naver.com>
2024-11-06 19:13:43 +0900

    ChatMessageBubble 컴포넌트 분리, ChatMessageMenu 추가
    TODO : ChatMessageMenu 관련 API 분석, 추가, 연결

--- src/app/view/main/content/Chat/MainChat/ChatArea.tsx
+++ src/app/view/main/content/Chat/MainChat/ChatArea.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useRef} from 'react';
 import {Box, Avatar} from '@mui/material';
 import styles from '@chats/Styles/StyleChat.module.css';
+import ChatMessageBubble from './ChatMessageBubble';
 
 interface Message {
   text: string;
@@ -59,79 +60,13 @@ const ChatArea: React.FC<ChatAreaProps> = ({messages, bgUrl, iconUrl, isHideChat
       >
         {isHideChat === false &&
           messages.Messages.map((msg, index) => (
-            <Box
-              key={index}
-              sx={{
-                display: 'flex',
-                justifyContent:
-                  msg.sender === 'user' || msg.sender === 'userNarration'
-                    ? 'flex-end'
-                    : msg.sender === 'partner' || msg.sender === 'narration'
-                    ? 'flex-start'
-                    : 'center',
-                marginBottom: 2,
-              }}
-            >
-              {msg.sender === 'partner' && (
-                <Avatar
-                  alt="Partner Avatar"
-                  src={iconUrl}
-                  sx={{
-                    width: 32,
-                    height: 32,
-                    marginRight: 1,
-                    border: '1px solid',
-                    borderColor: 'black',
-                  }}
-                />
-              )}
-              <Box
-                sx={{
-                  display: 'inline-block',
-                  padding: msg.sender === 'system' ? '8px 55px' : '8px',
-                  borderRadius: '8px',
-                  maxWidth: msg.sender === 'introPrompt' ? '100%' : msg.sender === 'system' ? '100%' : '70%',
-                  backgroundColor:
-                    msg.sender === 'introPrompt'
-                      ? '#FFFFFF'
-                      : msg.sender === 'user' || msg.sender === 'userNarration'
-                      ? 'rgba(80, 80, 80, 0.8)'
-                      : msg.sender === 'partner' || msg.sender === 'narration'
-                      ? 'rgba(0, 0, 0, 0.8)'
-                      : 'rgba(214, 214, 214, 0.2)',
-                  border: msg.sender === 'introPrompt' || msg.sender === 'system' ? '1px solid #C0C0C0' : 'none',
-                  backdropFilter: msg.sender === 'system' ? 'blur(20px)' : 'none',
-                  textAlign: msg.sender === 'narration' ? 'center' : 'inherit',
-                  color:
-                    msg.sender === 'introPrompt'
-                      ? '#000000'
-                      : msg.sender === 'system'
-                      ? '#FFFFFF'
-                      : msg.sender === 'narration' || msg.sender === 'userNarration'
-                      ? '#B0B0B0'
-                      : '#FFFFFF',
-                  fontSize: msg.sender === 'narration' || msg.sender === 'system' ? '0.7em' : '0.8em',
-                  fontWeight: msg.sender === 'system' ? 'bold' : 'normal',
-                  wordWrap: 'break-word',
-                  whiteSpace: 'pre-wrap',
-                  textShadow:
-                    msg.sender === 'system'
-                      ? '1px 1px 0 rgba(116, 116, 116, 1.0), -1px -1px 0 rgba(116, 116, 116, 1.0), 1px -1px 0 rgba(116, 116, 116, 1.0), -1px 1px 0 rgba(116, 116, 116, 1.0)'
-                      : 'none',
-                  marginLeft: msg.sender === 'narration' ? '40px' : '0px',
-                }}
-              >
-                {msg.sender === 'user' && messages.emoticonUrl[index] ? (
-                  <img
-                    src={messages.emoticonUrl[index]}
-                    alt="Emoticon"
-                    style={{width: '24px', height: '24px', marginTop: '4px'}}
-                  />
-                ) : (
-                  <div dangerouslySetInnerHTML={{__html: msg.text}} />
-                )}
-              </Box>
-            </Box>
+            <ChatMessageBubble
+              text={msg.text}
+              sender={msg.sender}
+              index={index}
+              iconUrl={iconUrl}
+              emoticonUrl={messages.emoticonUrl[index]}
+            />
           ))}
         <div ref={bottomRef} />
       </Box>
