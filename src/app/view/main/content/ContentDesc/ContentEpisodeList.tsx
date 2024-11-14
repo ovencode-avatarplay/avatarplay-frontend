db77c0279dd5aae05e7cd6ecd30c13457eae4857
BaeHyeonSeong <gustjd8121@gmail.com>
2024-11-14 17:29:51 +0900

    슬라이드 시 에피소드 id 바뀌게

--- src/app/view/main/content/ContentDesc/ContentEpisodeList.tsx
+++ src/app/view/main/content/ContentDesc/ContentEpisodeList.tsx
@@ -15,11 +15,22 @@ interface DrawerContentEpisodeItemListProps {
 }
 
 const DrawerContentEpisodeItemList: React.FC<DrawerContentEpisodeItemListProps> = ({episodes, onEpisodeSelect}) => {
+  const handleSlideChange = (swiper: any) => {
+    const activeIndex = swiper.activeIndex; // 현재 선택된 슬라이드의 인덱스를 가져옵니다.
+    onEpisodeSelect(activeIndex); // 선택된 인덱스를 onEpisodeSelect 함수에 전달합니다.
+  };
+
   return (
     <Box className={styles.episodeInfoList}>
-      <Swiper modules={[Pagination]} pagination={{clickable: true}} spaceBetween={'20px'} slidesPerView={1}>
+      <Swiper
+        modules={[Pagination]}
+        pagination={{clickable: true}}
+        spaceBetween={'20px'}
+        slidesPerView={1}
+        onSlideChange={handleSlideChange} // 슬라이드 변경 시 handleSlideChange를 호출합니다.
+      >
         {episodes.map((episode, index) => (
-          <SwiperSlide key={episode.episodeId} onClick={() => onEpisodeSelect(index)}>
+          <SwiperSlide key={episode.episodeId}>
             <ContentEpisodeItem
               episodeId={episode.episodeId}
               name={episode.name}
