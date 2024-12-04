ec2f22a948b84187bf7c58b76e91781478d2c47d
BaeHyeonSeong <gustjd8121@gmail.com>
2024-12-04 17:35:33 +0900

    TextField 자동완성 해제

--- src/app/Root.tsx
+++ src/app/Root.tsx
@@ -1,15 +1,26 @@
 'use client';
 
 import {store, persistor} from '@/redux-store/ReduxStore';
-import {ReactNode} from 'react';
+import {ReactNode, useEffect} from 'react';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
+import {ThemeProvider, createTheme} from '@mui/material/styles';
+
+const theme = createTheme({
+  components: {
+    MuiTextField: {
+      defaultProps: {
+        autoComplete: 'off',
+      },
+    },
+  },
+});
 
 const Root = ({children}: {children: ReactNode}) => {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        {children}
+        <ThemeProvider theme={theme}>{children}</ThemeProvider>
       </PersistGate>
     </Provider>
   );
