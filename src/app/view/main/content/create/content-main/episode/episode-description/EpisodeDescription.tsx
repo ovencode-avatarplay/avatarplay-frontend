905bb81c31581c934fe69e123e14ea519c73c124
XeDnA01 <ojb1112@gmail.com>
2024-12-04 14:25:32 +0900

    Auto stash before merge of "main" and "origin/main"

--- src/app/view/main/content/create/content-main/episode/episode-description/EpisodeDescription.tsx
+++ src/app/view/main/content/create/content-main/episode/episode-description/EpisodeDescription.tsx
@@ -13,6 +13,7 @@ import EpisodeConversationTemplate from '../episode-conversationtemplate/Episode
 
 import getLocalizedText from '@/utils/getLocalizedText';
 
+import MessageBox from '@/components/messageBox/MessageBox';
 import {string} from 'valibot';
 import MessageBox from '@/components/messageBox/MessageBox'; // 메시지박스
 
