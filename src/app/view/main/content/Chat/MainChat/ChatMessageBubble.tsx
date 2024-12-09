8d80a9f19795d89f208dbeaa5010deb92ca08754
tr8425 <tr8425@naver.com>
2024-12-09 15:59:59 +0900

    ChattingBubble, ContextMenu css

--- src/app/view/main/content/Chat/MainChat/ChatMessageBubble.tsx
+++ src/app/view/main/content/Chat/MainChat/ChatMessageBubble.tsx
@@ -98,144 +98,145 @@ const ChatMessageBubble: React.FC<ChatMessageBubbleProps> = ({
           }
         >
           <div className={styles.chatBubble}>
-            {selectedIndex === index && checkCanOpenContextTop() && <ChatMessageMenuTop id={id} />}
             <Box
               key={index}
               className={
                 sender === 'user' || sender === 'userNarration'
                   ? styles.chatBubbleJustifyUser
                   : sender === 'partner' || sender === 'partnerNarration'
-                  ? styles.chatBubbleJustifyPartner
+                  ? styles.chatBubbleProfilePartner
                   : styles.chatBubbleJustifySystem
               }
             >
               {(sender === 'partner' || sender === 'media') && (
                 <Avatar alt="Partner Avatar" src={iconUrl} className={styles.AvatarIcon} />
               )}
+              <Box className={styles.chatBubbleJustifyPartner}>
+                {selectedIndex === index && checkCanOpenContextTop() && <ChatMessageMenuTop id={id} />}
+                {sender !== 'media' && (
+                  <div
+                    className={
+                      sender === 'system'
+                        ? styles.chatBackSystem
+                        : sender === 'introPrompt'
+                        ? styles.chatBackIntro
+                        : sender === 'user'
+                        ? styles.chatBackUser
+                        : sender === 'userNarration'
+                        ? styles.chatBackUserNarration
+                        : sender === 'partner'
+                        ? styles.chatBackPartner
+                        : sender === 'partnerNarration'
+                        ? styles.chatBackPartnerNarration
+                        : styles.chatBackDefault
+                    }
+                    onClick={handleMenuOpen}
+                  >
+                    {sender === 'user' && emoticonUrl !== '' && emoticonUrl !== undefined ? (
+                      <img src={emoticonUrl} alt="Emoticon" style={{width: '24px', height: '24px', marginTop: '4px'}} />
+                    ) : (
+                      <div dangerouslySetInnerHTML={{__html: answerTextMessage}} />
+                    )}
+                  </div>
+                )}
+                {selectedIndex === index && checkCanOpenContextBottom() && (
+                  <ChatMessageMenuBottom
+                    text={text}
+                    id={id}
+                    onTtsClick={e => {
+                      handleTtsClick(e);
+                    }}
+                    // onDelete={handleDeleteAnswer}
+                    // onModified={handleAnswerModify}
+                    isUserChat={sender === 'user' || sender === 'userNarration'}
+                    lastMessageId={lastMessage.chatId}
+                  />
+                )}
+                {sender === 'media' && mediaData && mediaData.mediaType === TriggerMediaState.TriggerImage && (
+                  <Swiper
+                    effect={'cards'}
+                    grabCursor={false}
+                    modules={[EffectCards]}
+                    className={styles.mySwiper}
+                    style={{
+                      width: '100px', // 원하는 가로 크기
+                      height: 'auto', // 원하는 세로 크기
+                      marginLeft: '10%',
+                    }}
+                    loop={false} // 루프 비활성화 (슬라이드가 끝나면 멈춤)
+                    freeMode={false} // 자유 모드 비활성화
+                    allowSlideNext={false} // 슬라이드 이동 방지
+                    allowSlidePrev={false} // 슬라이드 이동 방지
+                    onClick={handleMediaClick}
+                  >
+                    {mediaData.mediaUrlList.map((url, idx) => (
+                      <SwiperSlide key={idx}>
+                        <img
+                          src={url}
+                          alt={`Media ${idx}`}
+                          style={{width: '100%', height: 'auto', borderRadius: '8px'}}
+                        />
+                      </SwiperSlide>
+                    ))}
+                  </Swiper>
+                )}
 
-              {sender !== 'media' && (
-                <div
-                  className={
-                    sender === 'system'
-                      ? styles.chatBackSystem
-                      : sender === 'introPrompt'
-                      ? styles.chatBackIntro
-                      : sender === 'user'
-                      ? styles.chatBackUser
-                      : sender === 'userNarration'
-                      ? styles.chatBackUserNarration
-                      : sender === 'partner'
-                      ? styles.chatBackPartner
-                      : sender === 'partnerNarration'
-                      ? styles.chatBackPartnerNarration
-                      : styles.chatBackDefault
-                  }
-                  onClick={handleMenuOpen}
-                >
-                  {sender === 'user' && emoticonUrl !== '' && emoticonUrl !== undefined ? (
-                    <img src={emoticonUrl} alt="Emoticon" style={{width: '24px', height: '24px', marginTop: '4px'}} />
-                  ) : (
-                    <div dangerouslySetInnerHTML={{__html: answerTextMessage}} />
-                  )}
-                </div>
-              )}
-              {selectedIndex === index && checkCanOpenContextBottom() && (
-                <ChatMessageMenuBottom
-                  text={text}
-                  id={id}
-                  onTtsClick={e => {
-                    handleTtsClick(e);
-                  }}
-                  // onDelete={handleDeleteAnswer}
-                  // onModified={handleAnswerModify}
-                  isUserChat={sender === 'user' || sender === 'userNarration'}
-                  lastMessageId={lastMessage.chatId}
-                />
-              )}
-              {sender === 'media' && mediaData && mediaData.mediaType === TriggerMediaState.TriggerImage && (
-                <Swiper
-                  effect={'cards'}
-                  grabCursor={false}
-                  modules={[EffectCards]}
-                  className={styles.mySwiper}
-                  style={{
-                    width: '100px', // 원하는 가로 크기
-                    height: 'auto', // 원하는 세로 크기
-                    marginLeft: '10%',
-                  }}
-                  loop={false} // 루프 비활성화 (슬라이드가 끝나면 멈춤)
-                  freeMode={false} // 자유 모드 비활성화
-                  allowSlideNext={false} // 슬라이드 이동 방지
-                  allowSlidePrev={false} // 슬라이드 이동 방지
-                  onClick={handleMediaClick}
-                >
-                  {mediaData.mediaUrlList.map((url, idx) => (
-                    <SwiperSlide key={idx}>
-                      <img
-                        src={url}
-                        alt={`Media ${idx}`}
-                        style={{width: '100%', height: 'auto', borderRadius: '8px'}}
+                {/* 비디오 출력 */}
+                {sender === 'media' && mediaData && mediaData.mediaType === TriggerMediaState.TriggerVideo && (
+                  <Box
+                    sx={{
+                      position: 'relative',
+                      width: '100%',
+                      height: 'auto', // 원하는 높이로 설정
+                    }}
+                  >
+                    {/* ReactPlayer */}
+                    {mediaData && mediaData.mediaType === TriggerMediaState.TriggerVideo && (
+                      <ReactPlayer
+                        muted={true}
+                        url={mediaData.mediaUrlList[0]} // 첫 번째 URL 사용
+                        width="70%" // 비율 유지하며 너비 자동 조정
+                        height="auto" // 비율 유지하며 높이 자동 조정
+                        style={{
+                          borderRadius: '8px',
+                        }}
                       />
-                    </SwiperSlide>
-                  ))}
-                </Swiper>
-              )}
+                    )}
 
-              {/* 비디오 출력 */}
-              {sender === 'media' && mediaData && mediaData.mediaType === TriggerMediaState.TriggerVideo && (
-                <Box
-                  sx={{
-                    position: 'relative',
-                    width: '100%',
-                    height: 'auto', // 원하는 높이로 설정
-                  }}
-                >
-                  {/* ReactPlayer */}
-                  {mediaData && mediaData.mediaType === TriggerMediaState.TriggerVideo && (
-                    <ReactPlayer
-                      muted={true}
-                      url={mediaData.mediaUrlList[0]} // 첫 번째 URL 사용
-                      width="70%" // 비율 유지하며 너비 자동 조정
-                      height="auto" // 비율 유지하며 높이 자동 조정
+                    {/* Play 버튼 */}
+                    <IconButton
+                      onClick={handleMediaClick}
+                      sx={{
+                        position: 'absolute',
+                        top: '50%',
+                        left: '35%', // ReactPlayer width 70%를 고려하여 중앙에 배치
+                        transform: 'translate(-50%, -50%)', // 중앙 정렬
+                        color: 'white', // 아이콘 색상
+                        fontSize: 48, // 아이콘 크기
+                        zIndex: 10, // 다른 요소 위에 표시
+                      }}
+                    >
+                      <PlayCircleIcon fontSize="inherit" />
+                    </IconButton>
+                  </Box>
+                )}
+                {/* 오디오 출력 */}
+                {sender === 'media' && mediaData && mediaData.mediaType === TriggerMediaState.TriggerAudio && (
+                  <Box
+                    style={{
+                      width: '100%',
+                    }}
+                  >
+                    <ReactAudioPlayer
+                      src={mediaData.mediaUrlList[0]}
+                      controls // 재생 컨트롤 활성화
                       style={{
                         borderRadius: '8px',
                       }}
                     />
-                  )}
-
-                  {/* Play 버튼 */}
-                  <IconButton
-                    onClick={handleMediaClick}
-                    sx={{
-                      position: 'absolute',
-                      top: '50%',
-                      left: '35%', // ReactPlayer width 70%를 고려하여 중앙에 배치
-                      transform: 'translate(-50%, -50%)', // 중앙 정렬
-                      color: 'white', // 아이콘 색상
-                      fontSize: 48, // 아이콘 크기
-                      zIndex: 10, // 다른 요소 위에 표시
-                    }}
-                  >
-                    <PlayCircleIcon fontSize="inherit" />
-                  </IconButton>
-                </Box>
-              )}
-              {/* 오디오 출력 */}
-              {sender === 'media' && mediaData && mediaData.mediaType === TriggerMediaState.TriggerAudio && (
-                <Box
-                  style={{
-                    width: '100%',
-                  }}
-                >
-                  <ReactAudioPlayer
-                    src={mediaData.mediaUrlList[0]}
-                    controls // 재생 컨트롤 활성화
-                    style={{
-                      borderRadius: '8px',
-                    }}
-                  />
-                </Box>
-              )}
+                  </Box>
+                )}
+              </Box>
             </Box>
           </div>
         </Box>
