f6c0dc2d65152306e85a342264f1b02a1a5d2822
tr8425 <tr8425@naver.com>
2024-12-12 11:39:19 +0900

    System Error - Chatting

--- src/app/NetWork/ESystemError.ts
+++ src/app/NetWork/ESystemError.ts
@@ -1,5 +1,5 @@
 export enum ESystemError {
-  syserr_chatting_send_post = '%Failed to send message. Please try again.%',
+  syserr_chatting_send_post = '%There is something wrong. Please try again.%',
 
   syserr_chat_stream_error = '%Stream encountered an error or connection was lost. Please try again.%',
 }
