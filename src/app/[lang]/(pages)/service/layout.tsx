05980c98abb36a2c2093f232ba5dcf0157a0bb79
XeDnA01 <ojb1112@gmail.com>
2025-01-08 11:33:38 +0900

    service page 추가

---
+++ src/app/[lang]/(pages)/service/layout.tsx
@@ -0,0 +1,13 @@
+import type {Metadata} from 'next';
+import '@/app/globals.css';
+import Service from '@/app/view/service/service';
+
+export const metadata: Metadata = {
+  title: 'Talkain',
+  description: 'Studio',
+  icons: '/images/Talkain_icon_256_green.png',
+};
+
+export default function RootLayout({children}: {children: React.ReactNode}) {
+  return <Service>{children}</Service>;
+}
