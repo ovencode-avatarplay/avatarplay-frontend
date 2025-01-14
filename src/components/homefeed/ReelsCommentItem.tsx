3f2fc4202dda7065ffce1dd65fc2b846ee73939d
BaeHyeonSeong <gustjd8121@gmail.com>
2025-01-14 09:08:46 +0900

    작업 중

--- src/components/homefeed/ReelsCommentItem.tsx
+++ src/components/homefeed/ReelsCommentItem.tsx
@@ -6,12 +6,11 @@ interface ReelsCommentItemProps {
   username: string;
   time: string;
   comment: string;
-  likes: string; // e.g., "2.3만"
-  dislikes: string; // e.g., "2"
-  replies: string; // e.g., "2"
+  likesCount: number; // e.g., "2.3만"
+  isLike: boolean; // e.g., "2"
 }
 
-const ReelsCommentItem: React.FC<ReelsCommentItemProps> = ({username, time, comment, likes, dislikes, replies}) => {
+const ReelsCommentItem: React.FC<ReelsCommentItemProps> = ({username, time, comment, likesCount, isLike}) => {
   return (
     <div className={styles.container}>
       <div className={styles.content}>
@@ -27,16 +26,19 @@ const ReelsCommentItem: React.FC<ReelsCommentItemProps> = ({username, time, comm
         {/* Actions */}
         <div className={styles.actions}>
           <div className={styles.actionItem}>
-            <img src={LineLike.src}></img>
-            <div className={styles.actionText}>{likes}</div>
-          </div>
-          <div className={styles.actionItem}>
-            <img src={LineDisLike.src}></img>
-            <div className={styles.actionText}>{dislikes}</div>
+            <img
+              src={LineLike.src}
+              className={styles.button}
+              style={{
+                filter: isLike
+                  ? 'brightness(0) saturate(100%) invert(47%) sepia(57%) saturate(1806%) hue-rotate(287deg) brightness(102%) contrast(98%)'
+                  : 'none', // 기본 상태는 필터 없음
+              }}
+            />
+            <div className={styles.actionText}>{likesCount.toString()}</div>
           </div>
           <div className={styles.actionItem}>
             <img src={LineComment.src}></img>
-            <div className={styles.actionText}>{replies}</div>
           </div>
         </div>
       </div>
