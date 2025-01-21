8c3492ebdfc2dfbb14ba65305d53e9d6f3de7e2e
tr8425 <tr8425@naver.com>
2025-01-21 10:46:28 +0900

    ExploreCard 관련 수정, ContentDesc - EpisodeItem 수정, Publishing 눌렀을 때 Studio/Story로 이동

--- src/app/view/main/content/searchboard/ExploreCard.tsx
+++ src/app/view/main/content/searchboard/ExploreCard.tsx
@@ -2,12 +2,12 @@ import React, {useState} from 'react';
 
 import styles from './ExploreCard.module.css'; // CSS 파일 임포트
 
-import { useDispatch } from 'react-redux';
-import { openDrawerContentId } from '@/redux-store/slices/DrawerContentDescSlice';
-import { openDrawerCharacterId } from '@/redux-store/slices/DrawerCharacterDescSlice';
+import {useDispatch} from 'react-redux';
+import {openDrawerContentId} from '@/redux-store/slices/DrawerContentDescSlice';
+import {openDrawerCharacterId} from '@/redux-store/slices/DrawerCharacterDescSlice';
 
-import { ExploreCardProps } from './SearchBoardTypes';
-import { BoldChatRoundDots, BoldEpisodes, BoldFollowers } from '@ui/Icons';
+import {ExploreCardProps} from './SearchBoardTypes';
+import {BoldChatRoundDots, BoldEpisodes, BoldFollowers} from '@ui/Icons';
 
 const ExploreCard: React.FC<ExploreCardProps> = ({
   exploreItemType,
@@ -71,6 +71,24 @@ const ExploreCard: React.FC<ExploreCardProps> = ({
     return styles[classType] || ''; // classType이 유효하지 않으면 빈 문자열 반환
   };
 
+  const formatNumber = (num: number) => {
+    if (num >= 1e18) {
+      return (num / 1e18).toFixed(1) + 'Qi'; // 10^18 (해)
+    } else if (num >= 1e15) {
+      return (num / 1e15).toFixed(1) + 'Qa'; // 10^15 (경)
+    } else if (num >= 1e12) {
+      return (num / 1e12).toFixed(1) + 'T'; // 10^12 (조)
+    } else if (num >= 1e9) {
+      return (num / 1e9).toFixed(1) + 'B'; // 10^9 (십억)
+    } else if (num >= 1e6) {
+      return (num / 1e6).toFixed(1) + 'M'; // 10^6 (백만)
+    } else if (num >= 1e3) {
+      return (num / 1e3).toFixed(1) + 'K'; // 10^3 (천)
+    } else {
+      return num.toString(); // 1000 미만은 그대로 출력
+    }
+  };
+
   return (
     <>
       <article className={`${styles.exploreCard} ${classType && getClassType(classType)}`}>
@@ -91,15 +109,17 @@ const ExploreCard: React.FC<ExploreCardProps> = ({
 
         <div className={styles.exploreOverlay}>
           <span className={styles.contentName}>{contentName}</span>
-          {updateExplorState !== 0 && <div className={styles.isNewLabel}>{getUpdateState(updateExplorState)}</div>}
+          {updateExplorState !== 0 && exploreItemType !== 1 && (
+            <div className={styles.isNewLabel}>{getUpdateState(updateExplorState)}</div>
+          )}
           <div className={styles.dataArea}>
             {exploreItemType === 0 ? (
               <>
-                {getDataItem(0, chatCount.toString())}
-                {getDataItem(1, episodeCount.toString())}
+                {getDataItem(0, formatNumber(chatCount))}
+                {getDataItem(1, formatNumber(episodeCount))}
               </>
             ) : (
-              <>{getDataItem(2, followerCount.toString())}</>
+              <>{getDataItem(2, formatNumber(followerCount))}</>
             )}
           </div>
         </div>
