1858c363c95cf501a7528287edee8112e8d3bbae
XeDnA01 <ojb1112@gmail.com>
2025-02-14 18:07:04 +0900

    리디렉션을 next.config에서 제거(주석)하고, middleware에서 하도록 함

--- next.config.mjs
+++ next.config.mjs
@@ -2,29 +2,29 @@
 const nextConfig = {
   reactStrictMode: false,
   basePath: process.env.BASEPATH || "", // basePath는 비워두거나 설정된 환경변수 사용
-  redirects: async () => {
-    return [
-      {
-        source: "/", // 루트 경로
-        destination: "/en-US/main/homefeed", // 기본 언어로 리다이렉트
-        permanent: true,
-        locale: false, // locale 처리를 하지 않도록 설정
-      },
-      {
-        source: "/:lang(ko|en-US|ja|fr|es|zh-CN|zh-TW|pt-PT|de)", // 각 언어별 경로
-        destination: "/:lang/main/homefeed", // 언어별 경로로 리디렉트
-        permanent: true,
-        locale: false,
-      },
-      {
-        source:
-          "/((?!(?:ko|en-US|ja|fr|es|zh-CN|zh-TW|pt-PT|de|front-pages|favicon.ico)\\b)):path", // 언어 코드가 없을 경우
-        destination: "/en-US/:path", // 기본 언어로 리다이렉트
-        permanent: true,
-        locale: false,
-      },
-    ];
-  },
+  // redirects: async () => {
+  //   return [
+  //     {
+  //       source: "/", // 루트 경로
+  //       destination: "/en-US/main/homefeed", // 기본 언어로 리다이렉트
+  //       permanent: true,
+  //       locale: false, // locale 처리를 하지 않도록 설정
+  //     },
+  //     {
+  //       source: "/:lang(ko|en-US|ja|fr|es|zh-CN|zh-TW|pt-PT|de)", // 각 언어별 경로
+  //       destination: "/:lang/main/homefeed", // 언어별 경로로 리디렉트
+  //       permanent: true,
+  //       locale: false,
+  //     },
+  //     {
+  //       source:
+  //         "/((?!(?:ko|en-US|ja|fr|es|zh-CN|zh-TW|pt-PT|de|front-pages|favicon.ico)\\b)):path", // 언어 코드가 없을 경우
+  //       destination: "/en-US/:path", // 기본 언어로 리다이렉트
+  //       permanent: true,
+  //       locale: false,
+  //     },
+  //   ];
+  // },
   typescript: {
     ignoreBuildErrors: true,
   },
