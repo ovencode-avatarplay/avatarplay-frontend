7106787eadb0328bc7c534aed80f13f4e15fdb55
hye <hye.sati@gmail.com>
2025-04-02 10:59:59 +0900

    로그인 버튼 width 수정, 햄버거바 수정

--- next.config.mjs
+++ next.config.mjs
@@ -29,6 +29,22 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: /\.[jt]sx?$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            icon: true,
+          },
+        },
+      ],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
