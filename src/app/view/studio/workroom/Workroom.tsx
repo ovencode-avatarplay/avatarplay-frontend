7824fae0236e9272242cd0e17f5d006732423977
tr8425 <tr8425@naver.com>
2025-04-15 19:05:51 +0900

    Workroom 작업중

--- src/app/view/studio/workroom/Workroom.tsx
+++ src/app/view/studio/workroom/Workroom.tsx
@@ -6,7 +6,9 @@ import Splitters from '@/components/layout/shared/CustomSplitter';
 import getLocalizedText from '@/utils/getLocalizedText';
 import {useState} from 'react';
 import WorkroomTagList from './WorkroomTagList';
-import WorkroomItem from './WorkroomItem';
+import {LineFolderPlus, LineSearch} from '@ui/Icons';
+import {Swiper, SwiperSlide} from 'swiper/react';
+import WorkroomItem, {WorkroomItemInfo} from './WorkroomItem';
 
 const Workroom: React.FC<Props> = ({}) => {
   //#region PreDefine
@@ -15,6 +17,69 @@ const Workroom: React.FC<Props> = ({}) => {
   const aiHistoryTags = ['All', 'Custom', 'Variation'];
   const galleryTags = ['All', 'MyCharacter', 'SharedCharacter'];
   const trashTags = ['All', 'Folders', 'Image', 'Video', 'Audio'];
+  //#endregion
+
+  //#region TmpDefine
+  // All 에서 보여지는 리스트는 20개 까지입니다.
+  const recentData: WorkroomItemInfo[] = [
+    {imgUrl: '/images/001.png', name: 'workroom0', detail: 'detail0'},
+    {imgUrl: '/images/001.png', name: 'workroom1', detail: 'detail1'},
+    {imgUrl: '/images/001.png', name: 'workroom2', detail: 'detail2'},
+    {imgUrl: '/images/001.png', name: 'workroom3', detail: 'detail3'},
+    {imgUrl: '/images/001.png', name: 'workroom4', detail: 'detail4'},
+    {imgUrl: '/images/001.png', name: 'workroom5', detail: 'detail5'},
+  ];
+
+  const folderData: WorkroomItemInfo[] = [
+    {imgUrl: '/images/001.png', name: 'folder0', detail: 'detail0'},
+    {imgUrl: '/images/001.png', name: 'folder1', detail: 'detail1'},
+    {imgUrl: '/images/001.png', name: 'folder2', detail: 'detail2'},
+    {imgUrl: '/images/001.png', name: 'folder3', detail: 'detail3'},
+    {imgUrl: '/images/001.png', name: 'folder4', detail: 'detail4'},
+    {imgUrl: '/images/001.png', name: 'folder5', detail: 'detail5'},
+    {imgUrl: '/images/001.png', name: 'folder6', detail: 'detail6'},
+    {imgUrl: '/images/001.png', name: 'folder7', detail: 'detail7'},
+    {imgUrl: '/images/001.png', name: 'folder8', detail: 'detail8'},
+    {imgUrl: '/images/001.png', name: 'folder9', detail: 'detail9'},
+    {imgUrl: '/images/001.png', name: 'folder10', detail: 'detail10'},
+    {imgUrl: '/images/001.png', name: 'folder11', detail: 'detail11'},
+    {imgUrl: '/images/001.png', name: 'folder12', detail: 'detail12'},
+    {imgUrl: '/images/001.png', name: 'folder13', detail: 'detail13'},
+    {imgUrl: '/images/001.png', name: 'folder14', detail: 'detail14'},
+    {imgUrl: '/images/001.png', name: 'folder15', detail: 'detail15'},
+    {imgUrl: '/images/001.png', name: 'folder16', detail: 'detail16'},
+    {imgUrl: '/images/001.png', name: 'folder17', detail: 'detail17'},
+    {imgUrl: '/images/001.png', name: 'folder18', detail: 'detail18'},
+    {imgUrl: '/images/001.png', name: 'folder19', detail: 'detail19'},
+    {imgUrl: '/images/001.png', name: 'folder20', detail: 'detail20'},
+  ];
+
+  const imageData: WorkroomItemInfo[] = [
+    {imgUrl: '/images/001.png', name: 'image0', detail: 'detail0'},
+    {imgUrl: '/images/001.png', name: 'image1', detail: 'detail1'},
+    {imgUrl: '/images/001.png', name: 'image2', detail: 'detail2'},
+    {imgUrl: '/images/001.png', name: 'image3', detail: 'detail3'},
+    {imgUrl: '/images/001.png', name: 'image4', detail: 'detail4'},
+    {imgUrl: '/images/001.png', name: 'image5', detail: 'detail5'},
+  ];
+
+  const videoData: WorkroomItemInfo[] = [
+    {imgUrl: '/images/001.png', name: 'video0', detail: 'detail0'},
+    {imgUrl: '/images/001.png', name: 'video1', detail: 'detail1'},
+    {imgUrl: '/images/001.png', name: 'video2', detail: 'detail2'},
+    {imgUrl: '/images/001.png', name: 'video3', detail: 'detail3'},
+    {imgUrl: '/images/001.png', name: 'video4', detail: 'detail4'},
+    {imgUrl: '/images/001.png', name: 'video5', detail: 'detail5'},
+  ];
+
+  const audioData: WorkroomItemInfo[] = [
+    {imgUrl: '/images/001.png', name: 'audio0', detail: 'detail0'},
+    {imgUrl: '/images/001.png', name: 'audio1', detail: 'detail1'},
+    {imgUrl: '/images/001.png', name: 'audio2', detail: 'detail2'},
+    {imgUrl: '/images/001.png', name: 'audio3', detail: 'detail3'},
+    {imgUrl: '/images/001.png', name: 'audio4', detail: 'detail4'},
+    {imgUrl: '/images/001.png', name: 'audio5', detail: 'detail5'},
+  ];
   //#endregion
 
   //#region State
@@ -34,23 +99,84 @@ const Workroom: React.FC<Props> = ({}) => {
   const renderMyWork = () => {
     return (
       <div className={styles.myWorkContainer}>
-        <div className={styles.recentArea}>recent</div>
-        {renderList()}
-        <div className={styles.folderArea}>folder</div>
-        <div className={styles.imageArea}>image</div>
-        <div className={styles.videoArea}>video</div>
-        <div className={styles.audioArea}>audio</div>
+        {tagStates.work === 'All' && (
+          <>
+            <div className={styles.listArea}>
+              <div className={styles.listTitleArea}>
+                <div className={styles.listTitle}>{getLocalizedText('TODO : Recent')}</div>
+              </div>
+              {renderSwiper(recentData)}
+            </div>
+            <div className={styles.listArea}>
+              <div className={styles.listTitleArea}>
+                <div className={styles.listTitle}>{getLocalizedText('TODO : Folder')}</div>
+                <div className={styles.listShowMore}>{getLocalizedText('TODO : Show more')}</div>
+              </div>
+              {renderSwiper(folderData)}
+            </div>
+            <div className={styles.listArea}>
+              <div className={styles.listTitleArea}>
+                <div className={styles.listTitle}>{getLocalizedText('TODO : Image')}</div>
+                <div className={styles.listShowMore}>{getLocalizedText('TODO : Show more')}</div>
+              </div>
+              {renderSwiper(imageData)}
+            </div>{' '}
+            <div className={styles.listArea}>
+              <div className={styles.listTitleArea}>
+                <div className={styles.listTitle}>{getLocalizedText('TODO : Video')}</div>
+                <div className={styles.listShowMore}>{getLocalizedText('TODO : Show more')}</div>
+              </div>
+              {renderSwiper(videoData)}
+            </div>{' '}
+            <div className={styles.listArea}>
+              <div className={styles.listTitleArea}>
+                <div className={styles.listTitle}>{getLocalizedText('TODO : Audio')}</div>
+                <div className={styles.listShowMore}>{getLocalizedText('TODO : Show more')}</div>
+              </div>
+              {renderSwiper(audioData)}
+            </div>
+          </>
+        )}
+
+        {tagStates.work === 'Folders' && renderGrid(folderData)}
+
+        {tagStates.work === 'Image' && renderGrid(imageData)}
+
+        {tagStates.work === 'Video' && renderGrid(videoData)}
+
+        {tagStates.work === 'Audio' && renderGrid(audioData)}
       </div>
     );
   };
 
-  const renderList = () => {
+  const renderSwiper = (data: WorkroomItemInfo[]) => {
     return (
-      <div className={styles.listContainer}>
-        <ul className={styles.listArea}>
-          <WorkroomItem detailView={detailView} />
-          <WorkroomItem detailView={detailView} />
-          <WorkroomItem detailView={detailView} />
+      <Swiper
+        slidesPerView={'auto'}
+        spaceBetween={5}
+        className={styles.workroomSwiper}
+        grabCursor={true}
+        // onSlideChange={handleSlideChange}
+      >
+        {data.map((item, index) => (
+          <SwiperSlide key={index}>
+            <WorkroomItem detailView={detailView} item={item} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    );
+  };
+
+  const renderGrid = (data: WorkroomItemInfo[]) => {
+    return (
+      <div className={styles.gridContainer}>
+        <div className={styles.filterArea}>FILTER</div>
+        <ul className={styles.gridArea}>
+          {data.map((item, index) => (
+            <div key={index}>
+              <WorkroomItem detailView={detailView} item={item} />
+            </div>
+          ))}
         </ul>
       </div>
     );
@@ -78,7 +204,7 @@ const Workroom: React.FC<Props> = ({}) => {
           onTagChange={tag => setTagStates(prev => ({...prev, favorite: tag}))}
         />
       ),
-      content: <>{renderList()}</>,
+      content: <>{}</>,
     },
     {
       label: getLocalizedText('TODO : AI history'),
@@ -89,7 +215,7 @@ const Workroom: React.FC<Props> = ({}) => {
           onTagChange={tag => setTagStates(prev => ({...prev, aiHistory: tag}))}
         />
       ),
-      content: <>{renderList()}</>,
+      content: <>{}</>,
     },
     {
       label: getLocalizedText('TODO : Gallery'),
@@ -100,7 +226,7 @@ const Workroom: React.FC<Props> = ({}) => {
           onTagChange={tag => setTagStates(prev => ({...prev, gallery: tag}))}
         />
       ),
-      content: <>{renderList()}</>,
+      content: <>{}</>,
     },
     {
       label: getLocalizedText('TODO : Trash'),
@@ -111,22 +237,25 @@ const Workroom: React.FC<Props> = ({}) => {
           onTagChange={tag => setTagStates(prev => ({...prev, trash: tag}))}
         />
       ),
-      content: <>{renderList()}</>,
+      content: <>{}</>,
     },
   ];
 
   return (
     <div className={styles.workroomContainer}>
-      <CreateDrawerHeader title="WORKROOM" onClose={() => {}}>
+      <CreateDrawerHeader title={getLocalizedText('TODO : Workroom')} onClose={() => {}}>
         {
           <div className={styles.buttonArea}>
-            <button>btnSearch</button>
-            <button>btnAdd</button>
+            <button className={styles.topButton}>
+              <img className={styles.buttonIcon} src={LineSearch.src} />
+            </button>
+            <button className={styles.topButton}>
+              <img className={styles.buttonIcon} src={LineFolderPlus.src} />
+            </button>
           </div>
         }
       </CreateDrawerHeader>
       <Splitters splitters={splitData} headerStyle={{padding: '0 16px'}} />
-      WORKROOM
     </div>
   );
 };
--- src/app/view/studio/workroom/WorkroomItem.tsx
+++ src/app/view/studio/workroom/WorkroomItem.tsx
@@ -1,18 +1,30 @@
+import {BoldMenuDots} from '@ui/Icons';
 import styles from './WorkroomItem.module.css';
 
 interface Props {
   detailView: boolean;
+  item: WorkroomItemInfo;
 }
 
-const WorkroomItem: React.FC<Props> = ({detailView}) => {
+export interface WorkroomItemInfo {
+  imgUrl: string;
+  name: string;
+  detail: string;
+}
+
+const WorkroomItem: React.FC<Props> = ({detailView, item}) => {
   const renderFileInfoArea = () => {
     return (
       <div className={styles.fileInfoArea}>
         <div className={styles.infoLeftArea}>
-          <div className={styles.fileName}></div>
-          <div className={styles.fileDetail}></div>
+          <div className={styles.fileName}>{item.name}</div>
+          <div className={styles.fileDetail}>{item.detail}</div>
         </div>
-        <div className={styles.infoRightArea}></div>
+        <div className={styles.infoRightArea}>
+          <button className={styles.btnMenu}>
+            <img className={styles.btnIcon} src={BoldMenuDots.src} />
+          </button>
+        </div>
       </div>
     );
   };
@@ -21,12 +33,28 @@ const WorkroomItem: React.FC<Props> = ({detailView}) => {
     <div className={styles.workroomItem}>
       {detailView ? (
         <div className={styles.detailViewContainer}>
-          <div className={styles.fileImage} style={{background: ''}} />
+          <div
+            className={styles.fileImage}
+            style={{
+              backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.0), rgba(0, 0, 0, 0.5)), url(${
+                item.imgUrl ? item.imgUrl : '/images/001.png'
+              })`,
+              backgroundSize: 'cover',
+            }}
+          />
           {renderFileInfoArea()}
         </div>
       ) : (
         <div className={styles.largeViewContainer}>
-          <div className={styles.fileImage} style={{background: ''}} />
+          <div
+            className={styles.fileImage}
+            style={{
+              backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.0), rgba(0, 0, 0, 0.5)), url(${
+                item.imgUrl ? item.imgUrl : '/images/001.png'
+              })`,
+              backgroundSize: 'cover',
+            }}
+          />
           {renderFileInfoArea()}
         </div>
       )}
